5d9dd6a9a83ec2d338974afbb49a5df84101fbdf
sanja7s <sanjascepanovic@gmail.com>
2017-12-22 16:14:55 +0200

    existing SAR sim for 3D and started my raw data sim

---
+++ chirp-signal-function.py
@@ -0,0 +1,5 @@
+def generateIdealChirp(chirpStartFreq,chirpStopFreq,chirpLength,rangeSamplingRate):
+    pulseDuration=chirpLength/rangeSamplingRate
+    k=(chirpStopFreq-chirpStartFreq)/pulseDuration
+    t=np.linspace(0,pulseDuration,chirpLength)
+    return np.asfortranarray(np.exp(1j*np.pi*(2*chirpStartFreq*t+k*t*t)).reshape(chirpLength,1)).astype(np.complex64)
